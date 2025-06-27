10fc54a4c4c8e44d6db5df60bd6fa3c69d992fd3
uliguapo1 <ulisescamelo933@gmail.com>
2025-06-27 13:00:53 -0600

    crear y revolver decks

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 const suits = ["♠", "♣", "♦", "♥"]; 
 const ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 
+function generateDeck() {
+    const deck = [];
+    for (let suit of suits) {
+        for (let rank of ranks) {
+            deck.push( rank + suit );
+        }
+    }
+    return deck;
+}
+
+function shuffleDeck(deck) {
+    for (let i = 0; i < deck.length; i++) {
+        let j = Math.floor(Math.random() * deck.length);
+        [deck[i], deck[j]] = [deck[j], deck[i]];
+    }
+}
+
